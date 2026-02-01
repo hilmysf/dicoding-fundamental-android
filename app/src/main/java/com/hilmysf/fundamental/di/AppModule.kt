38bcd65efa0f8413a29b51cc9ed6ee257e7bfc2c
hilmysf <hilmy.barca@gmail.com>
2026-02-01 23:08:33 +0700

    feat(di): add AppModule with DataStore and SettingPreferences providers

---
+++ app/src/main/java/com/hilmysf/fundamental/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.hilmysf.fundamental.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.hilmysf.fundamental.data.local.SettingPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            produceFile = { context.preferencesDataStoreFile("settings") }
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideSettingPreferences(dataStore: DataStore<Preferences>): SettingPreferences {
+        return SettingPreferences.getInstance(dataStore)
+    }
+}
